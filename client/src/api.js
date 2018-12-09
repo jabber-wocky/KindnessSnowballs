2616bfb43e297d93084fb45ad86b379e70994f6e
Nicholas O'Hara <Nicholas.O'Hara@086gc.onmicrosoft.com>
2018-12-08 21:40:45 -0500

    update api location based on node_env

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
     return axios.create({
-        baseURL: `http://localhost:8081/api`,
+        baseURL: process.env.NODE_ENV === 'production' ?  '/api' : `http://localhost:8080/api`,
         withCredentials: false,
         headers: {
             'Accept': 'application/json',
