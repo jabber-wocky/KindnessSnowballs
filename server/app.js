ea86c093feda2f4e92114250d3b134b3f096f24c
Nicholas O'Hara <Nicholas.O'Hara@086gc.onmicrosoft.com>
2018-12-08 21:00:23 -0500

    added express port

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = require('morgan')
 const appsettings = require('./appsettings.json')
 
 const app = express()
+app.set('port', process.env.PORT || 3000)
 app.use(express.static('dist'))
-app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -101,4 +100,4 @@ app.post(`/api/stories`, async (req, res) => {
   }
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(app.get('port'))
