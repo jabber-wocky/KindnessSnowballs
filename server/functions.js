7b738314308e424d3d705b0a278757d7da41267d
Nicholas O'Hara <nicholas.ohara@dfo-mpo.gc.ca>
2018-12-31 10:30:00 -0500

    fixed connection string

--- server/functions.js
+++ server/functions.js
@@ -28,8 +28,8 @@ module.exports = (() => {
       var mongoClient = require("mongodb").MongoClient
       
       var connectionString = process.env.MONGODB_URI !== "" ? process.env.MONGODB_URI : appsettings.connectionString
-      
-      return mongoClient.connect(appsettings.connectionString, { useNewUrlParser: true })
+      console.log("using connection string", connectionString)
+      return mongoClient.connect(connectionString, { useNewUrlParser: true })
     }
   }
 })();
